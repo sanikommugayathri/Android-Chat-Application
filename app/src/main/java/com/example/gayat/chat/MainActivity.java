ee274f28272725fc8f48bb9482c07ac7cbebf220
sanikommugayathri <sanikommugayathri@gmail.com>
2019-01-02 20:32:35 -0600

    Added Group chat functionality

--- app/src/main/java/com/example/gayat/chat/MainActivity.java
+++ app/src/main/java/com/example/gayat/chat/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.gayat.chat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -118,12 +125,72 @@ public class MainActivity extends AppCompatActivity {
         {
             SendUserToSettingsActivity();
         }
+        if(item.getItemId() == R.id.main_create_group_option)
+        {
+            RequestNewGroup();
+        }
         if(item.getItemId() == R.id.main_find_friends_option)
         {
 
         }
 
         return true;
+    }
+
+    private void RequestNewGroup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialog);
+        builder.setTitle("Enter Group Name:");
+
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("e.g Fight Club");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                String groupName = groupNameField.getText().toString();
+
+                if(TextUtils.isEmpty(groupName))
+                {
+                    Toast.makeText(MainActivity.this, "Please enter a group name", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    CreateNewGroup(groupName);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                dialogInterface.cancel();
+
+            }
+        });
+
+        builder.show();
+
+    }
+
+    private void CreateNewGroup(final String groupName)
+    {
+        RootRef.child("Groups").child(groupName).setValue("").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(MainActivity.this, groupName + " is created successfully", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     private void SendUserToLoginActivity()
--- app/src/main/java/com/example/gayat/chat/SettingsActivity.java
+++ app/src/main/java/com/example/gayat/chat/SettingsActivity.java
@@ -13,8 +13,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -42,6 +45,8 @@ public class SettingsActivity extends AppCompatActivity
 
         InitializeFields();
 
+        userName.setVisibility(View.INVISIBLE);
+
         UpdateAccountSettings.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -51,6 +56,8 @@ public class SettingsActivity extends AppCompatActivity
 
             }
         });
+
+        RetrieveUserInfo();
     }
 
 
@@ -100,6 +107,47 @@ public class SettingsActivity extends AppCompatActivity
                 }
             });
         }
+    }
+
+
+    private void RetrieveUserInfo()
+    {
+        RootRef.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if((dataSnapshot.exists()) && (dataSnapshot.hasChild("name") && (dataSnapshot.hasChild("image"))))
+                {
+                    String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                    String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+                    String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
+
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+                }
+                else if((dataSnapshot.exists()) && (dataSnapshot.hasChild("name")))
+                {
+                    String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                    String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+                    String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
+
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+                }
+                else
+                {
+                    userName.setVisibility(View.VISIBLE);
+                    Toast.makeText(SettingsActivity.this,"Please set and update your profile information", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void SendUserToMainActivity()
